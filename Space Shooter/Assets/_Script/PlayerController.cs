3b8b0a88a7cd91957caefbf2131bc2f446c36743
suzit <bsuzit@gmail.com>
2017-04-24 14:10:26 +0600

    Added Initial project
    Unity Game

---
+++ Space Shooter/Assets/_Script/PlayerController.cs
@@ -0,0 +1,68 @@
+﻿using UnityEngine;
+using System.Collections;
+[System.Serializable]
+public class Boundary{
+	public float xMin,xMax,zMin,zMax;
+}
+
+public class PlayerController : MonoBehaviour {
+	public float speed;
+	public float tilt;
+	public GameObject shot;
+	public float fireRate;
+	
+	private float nextFire;
+	public Transform shotSpawn;//shotSpawn.transform.position(GameObject)
+	public Boundary boundary;
+	private Quaternion calibrationQuaternion;
+	public SimpleTouchPad touchPad;
+	public SimpleTouchAreaButton areaButton;
+	void Start(){
+		CalibrateAccelerometer ();
+	}
+	void Update(){
+		//Instantiate(object,postion,rotation);
+		//Instantiate (shot, shotSpawn, shotSpawn.rotation);
+		if (areaButton.CanFire () && Time.time > nextFire) 
+		{
+			nextFire = Time.time + fireRate;
+			Instantiate(shot, shotSpawn.position, shotSpawn.rotation);
+			GetComponent<AudioSource>().Play();
+		}
+	}
+	void CalibrateAccelerometer (){
+		//Used to calibrate the Iput.acceleration input
+		Vector3 accelerationSnapshot = Input.acceleration;
+		Quaternion rotateQuaternion = Quaternion.FromToRotation (new Vector3 (0.0f, 0.0f, -1.0f), accelerationSnapshot);
+		calibrationQuaternion = Quaternion.Inverse (rotateQuaternion);
+
+	}
+	Vector3 FixAcceleration(Vector3 acceleration){
+		Vector3 fixedAcceleration = calibrationQuaternion * acceleration;
+		return fixedAcceleration;
+	}
+
+	void FixedUpdate(){
+		//float moveHorizontal = Input.GetAxis ("Horizontal");//GetAxis returns 0 and 1 only
+		//float moveVertical = Input.GetAxis ("Vertical");
+		//Vector3 movement = new Vector3 (moveHorizontal, 0.0f, moveVertical);
+
+		//Vector3 accelerationRaw = Input.acceleration;
+		//Vector3 movement = new Vector3 (acceleration.x,0.0f,acceleration.y);
+		//Vector3 acceleration = FixAcceleration (accelerationRaw);
+		Vector2 direction = touchPad.GetDirection ();
+		Vector3 movement = new Vector3 (direction.x,0.0f,direction.y);
+		GetComponent<Rigidbody>().velocity = movement*speed;
+		GetComponent<Rigidbody> ().position = new Vector3 (
+
+			Mathf.Clamp(GetComponent<Rigidbody>().position.x,boundary.xMin,boundary.xMax), 
+			0.0f, 
+			Mathf.Clamp(GetComponent<Rigidbody>().position.z,boundary.zMin,boundary.zMax)
+			);
+		GetComponent<Rigidbody> ().rotation = Quaternion.Euler (0.0f, 0.0f, GetComponent<Rigidbody> ().velocity.x * -tilt);//velocity left to right
+		//Quaternion Euler rotate x,y,z axix
+
+             }
+
+
+}
---
+++ Space Shooter/Assets/_Script/SimpleTouchPad.cs
@@ -0,0 +1,52 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.EventSystems;
+using System.Collections;
+
+public class SimpleTouchPad : MonoBehaviour,IPointerDownHandler,IDragHandler,IPointerUpHandler {
+	private Vector2 origin;
+	private Vector2 direction;
+	private bool touched;
+	private int pointerID;
+	private Vector2 smoothDirection;
+	public float smoothing;
+	void Awake () {
+		direction = Vector2.zero;
+		touched = false;
+		//touched = false;
+	}
+	public void OnPointerDown (PointerEventData data){
+		if (!touched) {
+			touched = true;
+			pointerID = data.pointerId;
+			origin = data.position;
+		}
+
+	}
+	public void OnDrag (PointerEventData data){
+		//compare the difference between start point and current point pos
+		if (data.pointerId == pointerID) {
+			Vector2 currentPosition = data.position;
+			Vector2 directionRaw = currentPosition - origin;
+			direction = directionRaw.normalized;
+		}
+		//Debug.Log (direction);
+
+
+	}
+	public void OnPointerUp (PointerEventData data){
+		//reset everything
+		if (data.pointerId == pointerID) {
+		direction = Vector2.zero;
+		touched = false;
+		}		
+
+	}
+	public Vector2 GetDirection ()  {
+			smoothDirection = Vector2.MoveTowards (smoothDirection, direction, smoothing);
+		    return smoothDirection;
+		//return direction;
+	}
+
+
+}
