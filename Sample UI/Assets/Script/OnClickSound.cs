5f04f68e737622fb897d55582805f61d190140c1
suzit <bsuzit@gmail.com>
2017-05-16 12:08:48 +0600

    User Interface
    Simple 2D Interface

---
+++ Sample UI/Assets/Script/OnClickSound.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+[RequireComponent(typeof(Button))]
+public class OnClickSound : MonoBehaviour {
+
+	public AudioClip sounds;
+	private Button button { get { return GetComponent<Button> (); } }
+	private AudioSource source { get { return GetComponent<AudioSource> (); } }
+	void Start()
+	{
+		gameObject.AddComponent<AudioSource> ();
+		source.clip = sounds;
+		source.playOnAwake = false;
+		button.onClick.AddListener (() => PlaySound ());
+		//button.onClick.AddListener (() => StopSound ());
+	}
+
+	void PlaySound(){
+		source.PlayOneShot (sounds);
+	}
+
+}
